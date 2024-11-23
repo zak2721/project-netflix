df84516867f906c174bcf6ca23ed22693d26abe6
zak2721 <zekiostheone@gmail.com>
2024-11-23 04:34:14 +0300

    first commit

---
+++ project 1/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='app'>
+        <h1>Zekios</h1>
+    </div>
+  )
+}
+
+export default App
